a12acec70f503e76dd64a98a25a46905ae48a99d
Wilbur <lixiaoyanghh@outlook.com>
2025-04-25 00:42:12 +0800

    feat(view): 优化SimpleView数据显示格式并增强异常处理

--- src/weather_tools/view/simple_view.py
+++ src/weather_tools/view/simple_view.py
@@ -4,15 +4,26 @@ class SimpleView:
 
 
     def view(self,data):
+        """
+        显示天气数据的视图方法
+        Args:
+            data: 包含天气信息的数据字典
+        """
         view_type = self.view_type
-        print(view_type)
-        #print(data)
-        for d in data:
-            for d1 in d.values():
+        print(view_type)  # 打印视图类型
+
+        # 遍历数据源中的值
+        for z in zip(data[0].values(),data[1].values()):
+            for d in z:
                 try:
-                    for k,v in d1['now'].items():
-                        print(k,v)
+                    # 尝试打印实时天气数据
+                    print('*' * 20)  # 分隔线
+                    for k,v in d['now'].items():
+                        print(k,v)  # 打印键值对
+                    print('*' * 20)  # 分隔线
                 except KeyError as e:
-                    print('Missing key:',e)
-                    for k,v in d1.items():
-                        print(k,v)
+                    # 如果没有'now'键,直接打印整个字典
+                    print('#' * 20)  # 分隔线
+                    for k,v in d.items():
+                        print(k,v)  # 打印键值对
+                    print('#' * 20)  # 分隔线
