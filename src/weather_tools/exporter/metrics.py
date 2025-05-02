e3d800f8447e2f2b8e0055f9b7e4615dd3d0b36d
Wilbur <lixiaoyanghh@outlook.com>
2025-05-02 12:39:01 +0800

    refactor(exporter): 重命名指标导出文件并调整模块结构

--- src/weather_tools/exporter/metrics.py
+++ src/weather_tools/exporter/metrics.py
@@ -5,6 +5,3 @@ real_temp = Gauge('qweather_real_temp',"和风天气实时温度")
 requests_counter = Counter('qweather_requests_counter',"API请求次数")
 weather_status = Summary('qweather_weather_status',"天气状态")
 
-
-if __name__ == '__main__':
-    start_http_server(8000)
