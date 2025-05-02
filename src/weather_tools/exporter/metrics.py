2b0291e28ef18d7ba8413b23fc88216e2b07960b
Wilbur <lixiaoyanghh@outlook.com>
2025-05-02 17:45:14 +0800

    feat(exporter): 新增指标服务器启动功能

--- src/weather_tools/exporter/metrics.py
+++ src/weather_tools/exporter/metrics.py
@@ -5,3 +5,6 @@ real_temp = Gauge('qweather_real_temp',"和风天气实时温度")
 requests_counter = Counter('qweather_requests_counter',"API请求次数")
 weather_status = Summary('qweather_weather_status',"天气状态")
 
+def start_metrics_server(port):
+    start_http_server(port)
+    print("metrics server start at port {}".format(port))
